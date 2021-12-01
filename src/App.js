56582adef28742b9bd08fb13c1048559c4039d27
fishka9044 <79632566+fishka9044@users.noreply.github.com>
2021-12-01 14:29:08 +0300

    createElem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const h1 = React.createElement('h1', {}, 'unit_08');
+
+  const h2 = React.createElement('h2', {'style':{
+    'color': 'orange',
+  }}, 'header 2');
+
+  const p = React.createElement('p', {'style':{
+    'color': 'red',
+  }}, 'this is p');
+
+  const input = React.createElement('input',{'defaultValue': 55}, )
+
+  const p1 = React.createElement('p', {}, 'hi');
+
+  const p2 = React.createElement('p', {}, 'world');
+
+  const div = React.createElement('div', {'className':'text-grey'}, p1, p2);
+
+
+
+  let ref = React.createRef();
+
+  let [m,setM] = useState([]);
+
+
+
+  function getValueInput(evt) {
+
+
+    evt.preventDefault();
+    const cE = React.createElement;
+    let value = ref.current.value;
+    let liItem = [...m, cE('li', {'key': value}, value)];
+    
+    if (ref.current.value !== '') {
+      setM(liItem);
+      ref.current.value =''
+    } else {
+      return false;
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {h1}
+      {h2}
+      {p}
+      {input}
+      {div}
+
+      <form action="">
+        <input ref={ref} type="text" />
+        <button onClick={getValueInput}>получить значение</button>
+      </form>
+
+      <ul>
+        {m}
+      </ul>
+
+    </>
   );
+
+  
 }
 
 export default App;
